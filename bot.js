c5247f3107c3a9cf101c856868c8aa90f69a7a64
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-02-14 19:02:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = ".";
 var adminprefix = '.'
-const developers = ['545655818645798915','545655818645798915']
+const developers = ["545655818645798915"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
