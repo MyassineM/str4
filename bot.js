d83b71c4676019309fcdf4223edc2dbd41a7e9be
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-04-05 11:59:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,42 +61,153 @@ client.on("message", (message) => {
  
 });
 
+client.on("message", message => {
+    if (message.content.startsWith("!bc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n ${m}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
+  message.delete();
+  };
+  });
 
 client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'setp')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**Bot Playing By  !                     | Yas.sine**#3327   ${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'setw')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**Bot Watching By !                     | Yas.sine**#3327   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'setl')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**Bot Listening By !                     | Yas.sine**#3327   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'sets')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/One");
-      message.channel.send(`**Bot Streaming By !                     | Yas.sine**#3327`)
-  }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`**Bot Change Name By !                     | Yas.sine**#3327 : ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setavatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`**Bot Change Avatar By !                     | Yas.sine**#3327: :**${argresult}** `);
+    if (message.content.startsWith("!bot")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .setTitle('``INFO Broadcast Arab Bot`` ')
+            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
+            .addField('``servers``', [client.guilds.size], true)
+            .addField('``channels``' , `[ ${client.channels.size} ]` , true)
+            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
+            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
+            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
+                  .addField('``My Prefix``' , `[-]` , true)
+                  .addField('``My Language``' , `[ Java Script ]` , true)
+                    })
 }
 });
 
+client.on('message', msg => {
+  if(msg.content === 'هلا')
+  msg.reply('هلا بيك')
+});
 
+client.on('message', msg => {
+  if(msg.content === 'سلام عليكم')
+  msg.reply('وعليكم السلام')
+});
 
+client.on('message', msg => {
+  if(msg.content === 'مين صاحب البوت')
+  msg.reply('دماء ')
+});
+
+client.on('message', msg => {
+  if(msg.content === 'اما')
+  msg.reply('والله ')
+});
+
+client.on('message', msg => {
+  if(msg.content === 'دماء')
+  msg.reply('شتبي منو')
+});
+
+client.on('message', msg => {
+  if(msg.content === 'سلام')
+  msg.reply('سلام حب ❤')
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix)) return;
+  if(!message.channel.guild) return message.reply('** This command only for servers **')
+    let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+    if (command === "kill"){
+
+   var sabotage = message.mentions.users.first();
+   if(sabotage == message.author)return message.reply(`**الانتحار مو زين و الله**`);
+    if(sabotage === client.user) return message.reply(`** تبي تقتلني ؟ **`);
+  if (sabotage < 1) {
+    message.delete();
+    return message.channel.sendMessage('ضع شيئا للقتل، مثل ذكر مستخدم، أو استخدام رمز تعبيري');
+  }
+  if (!sabotage) return message.channel.send(`Please Mention A Member to Kill :warning:`)
+  message.channel.send("▄︻̷̿┻̿═━一 ${sabotage")
+  .then(msg =>{
+  msg.edit(`▄︻̷̿┻̿═━一 ${sabotage} :three:`);
+  setTimeout(function() {
+    msg.edit(`▄︻̷̿┻̿═━一 ${sabotage} :two:`);
+  }, 1000);
+  setTimeout(function() {
+    msg.edit(`▄︻̷̿┻̿═━一 ${sabotage} :one:`);
+  }, 2000);
+  setTimeout(function() {
+    msg.edit(`▄︻̷̿┻̿═━一 :boom:`);
+  }, 3000);
+  setTimeout(function() {
+    msg.edit(`▄︻̷̿┻̿═━一 :fire:`);
+  }, 4000);
+  setTimeout(function() {
+    msg.edit(`▄︻̷̿┻̿═━一 :skull:`);
+  }, 5000);
+  msg.delete(6000)
+  message.delete()
+  })
+  message.channel.send("**تم اخفاء الجريمة بنجاح :hole: **").then(msg => msg.delete(10000));
+    }
+});
+
+client.on('message', message=> {
+    if (message.author.bot) return;
+    if (message.isMentioned(client.user))
+    {
+    message.reply(`**شتبي ليش منشنتني خلني بسيرفر ورع خخخخخخخ الله يعديك**`)
+    }
+});
+client.on('message', message => {
+  var prefix = "!"
+          if(message.content.startsWith(prefix + 'skin')) {
+              let args = message.content.split(' ').slice(1).join(' ');
+              if (!args) return message.channel.send("**من فضلك ضع إسمك**");
+              var link = (`https://minotar.net/body/${args}/100.png`);
+              message.channel.send(link);
+          }
+      });
+
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '436918120184021012') return;
+
+if (message.content.startsWith(prefix + 'p')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 'w')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(prefix + 'l')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark: : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 's')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+
+});
 
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
