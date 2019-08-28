9d5c5a255c0506215ad4eb57af9ae8e3cba3db9e
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-08-28 20:19:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,21 +67,21 @@ client.on('message', message => {
   var argresult = args.join(' ');
   if (message.author.id !== '436918120184021012') return;
 
-if (message.content.startsWith(prefix + 'p')) {
+if (message.content.startsWith(prefix + '1')) {
   client.user.setGame(argresult);
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 } else 
 
-if (message.content.startsWith(prefix + 'w')) {
+if (message.content.startsWith(prefix + '2')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 } else 
-if (message.content.startsWith(prefix + 'l')) {
+if (message.content.startsWith(prefix + '3')) {
 client.user.setActivity(argresult, {type:'LISTENING'});
     message.channel.sendMessage(`**:white_check_mark: : ${argresult}**`)
 } else 
 
-if (message.content.startsWith(prefix + 's')) {
+if (message.content.startsWith(prefix + '4')) {
   client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 }
