0a3aa272a9203b3da0abf0e994c7bad42f4ecdbf
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-08-30 12:56:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,22 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const Discord = require('discord.js');
+
+const Util = require('discord.js');
+
+const getYoutubeID = require('get-youtube-id');
+
+const fetchVideoInfo = require('youtube-info');
+
+const YouTube = require('simple-youtube-api');
+
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+
+const queue = new Map();
+
+const ytdl = require('ytdl-core');
+
+const fs = require('fs');
+
+const gif = require("gif-search");
 var prefix = "!";
 var adminprefix = '!'
 const developers = ["436918120184021012"]
