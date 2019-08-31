5635ed8dea06febd326a36339e5177ff1d497b22
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-08-31 20:48:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2219,4 +2219,64 @@ message.channel.awaitMessages(filte, { max: 1, time: 30000, errors: ['time'] })
   }
 });
 
+client.on('message', message => {
+    let prefix = '!';
+if(message.content.startsWith(prefix + "tempmute")){
+    let muteduser = message.mentions.members.first();
+    let jif = message.content.split(' ').slice(1);
+    let durration = jif[1];
+    let reason = message.content.split(' ').slice(3).join(' ');
+    let hh;
+    let muted = message.guild.roles.find(r => r.name === 'Muted');
+    if(!muteduser){
+        return message.channel.send('**#- I cannot find this guy**');
+    }
+    if(!message.guild.me.hasPermission('ADMINISTRATOR')){
+        return message.channel.send(`**#- I must have the \`ADMINISTRATOR\` Perms so i can mute people**`);
+    }
+    if(muteduser.hasPermission('ADMINISTRATOR')) {
+        return message.channel.send(`**#- He has a \`ADMINISTRATOR\` Perms and u cannot mute him**`);
+    }
+    if(!message.member.hasPermission('ADMINISTRATOR')){
+        return message.channel.send('**#- You must have \`ADMINISTRATOR\` Perms.**');
+    }
+    if(muteduser.id === message.author.id){
+        return message.channel.send(`**#- You can't mute yourself**`);
+    }
+    if(durration && !durration.match(/[1,10][s,m,h,d,w]/g)){
+        return message.channel.send('**#- Submit a right durration. \n Must be like the following submitation : 1-10 s = second , m = minute , h = hour , d = days, w = weeks**');
+    }
+    if(!muted){
+        return message.guild.createRole({name: 'Muted'});
+    }
+    if(!reason){
+       hh = null;
+    } else {
+        hh = reason;
+    }
+    if(hh === null){
+        hh = "No reason detected";
+    }
+    message.channel.send(`**${muteduser} Got muted :white_check_mark: \n Durration : ${durration}\n Reason : ${hh}**`);
+    console.log(mms(durration));
+    message.guild.channels.filter(m => m.type === 'text').forEach(f => {
+                      f.overwritePermissions(muted, {
+            SEND_MESSAGES: false
+        });
+    });
+    message.guild.channels.filter(s => s.type === 'voice').forEach(h => {
+                      h.overwritePermissions(muted, {
+            SPEAK: false
+        });
+    });
+    muteduser.addRole(muted).then(setTimeout(() => {
+    muteduser.removeRole(muted);
+    message.channel.send(`**${muteduser} Finally got unmuted :white_check_mark:**`);
+}, mms(durration)));
+ 
+    
+
+}
+});
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شي
