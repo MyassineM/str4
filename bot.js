21234477d2f9dbb5ee2cf251bed016c66e2e1d19
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-08-31 19:47:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2145,4 +2145,24 @@ client.on('message', message => {
     }
 });
 
+client.on('ready',async () => {
+  let GUILDID = '605524619268718592'; // اي دي السيرفر  let CHANNELID = '605524619268718596'; // اي دي الروم
+  voiceStay(GUILDID, CHANNELID);
+  function voiceStay(guildid, channelid) {
+    if(!guildid) throw new Error('Syntax: voiceStay function requires guildid');
+    if(!channelid) throw new Error('Syntax: voiceStay function requires channelid');
+
+    let guild = client.guilds.get(guildid);
+    let channel = guild.channels.get(channelid);
+
+    if(channel.type === 'voice') {
+      channel.join().catch(e => {
+        console.log(`Failed To Join :: ${e.message}`);
+      });
+    } else {
+      console.log(`Channel Type ::  ${channel.type}, It must be Voice.`);
+    }
+  }
+});
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شي
