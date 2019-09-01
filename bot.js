edbd6ef7d99eddbe79ee1e4799d0469f7d56b8f2
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-09-01 12:28:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,8 +22,6 @@ const fs = require('fs');
 
 const gif = require("gif-search");
 
-const reportjson = JSON.parse(fs.readFileSync('./report.json' , 'utf8'));
-
 let vipKeys = JSON.parse(fs.readFileSync("./vipKeys.json", "utf8"));
 
 const client = new Discord.Client();
