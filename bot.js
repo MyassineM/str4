54777dc196cbc8dc4f7a1ae8bf1cb2a7f44ff8bc
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-09-02 18:52:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,6 +31,8 @@ const reportjson = JSON.parse(fs.readFileSync("./report.json", "utf8"));
 
 const Captcha = JSON.parse(fs.readFileSync("./Captcha.json","utf8"));
 
+const afk = require('./afk.json');
+
 let vipKeys = JSON.parse(fs.readFileSync("./vipKeys.json", "utf8"));
 
 const client = new Discord.Client();
@@ -2679,4 +2681,39 @@ client.on("message", message => {
 }
 })
 
-client.login(process.env.BOT_TOKEN);// لا تغير فيها شي
+client.on('message',async rebel => {
+      if(rebel.author.bot) return;
+  if (afk[rebel.author.id]) {
+    delete afk[rebel.author.id];
+    if (rebel.member.nickname === null) {
+      msg.channel.send("**Welcome , <@"+rebel.author.id+"> I am trying to remove the afk from you.**");     } else {
+      rebel.member.setNickname(rebel.member.nickname.replace(/(\[AFK\])/,''));
+      rebel.channel.send("**Welcome , <@"+rebel.author.id+"> The afk was removed from you because you come back.**"); 
+    }
+    fs.writeFile("./afk.json", JSON.stringify(afk), (err) => {if (err) console.error(err);});
+} else {
+    if (rebel.content.startsWith(prefix + 'afk ')||rebel.content === prefix + 'afk') {
+      rebel.member.setNickname("[AFK] " + rebel.member.displayName);
+      let args1 = rebel.content.split(' ').slice(1);
+      if (args1.length === 0) {
+        afk[rebel.author.id] = {"reason": true}; 
+        rebel.reply("** I put you in a afk position **")
+      } else {
+        afk[rebel.author.id] = {"reason": args1.join(" ")}; // with reason
+        rebel.reply("**I added you to your afk because** "+ args1.join(" ") + ".")
+      }
+      fs.writeFile("./afk.json", JSON.stringify(afk), (err) => {if (err) console.error(err);});   
+  }
+}
+         var mentionned = rebel.mentions.users.first();
+if(rebel.mentions.users.size > 0) return ;
+if (afk[rebel.mentions.users.first().id]) {
+if (afk[rebel.mentions.users.first().id].reason === true) {
+rebel.channel.send(`**<@!${mentionned.id}> Afk** `);
+}else{
+rebel.channel.send(`**<@!${mentionned.username}> is Afk , Afk reason \n ${afk[rebel.mentions.users.first().id].reason}**`);
+}
+} 
+});
+
+client.login(process.env.BOT_TOKEN);// Mrbloods bot
