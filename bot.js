0d8a77026bf046aff90362ab234ec981c620ea8d
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-09-02 18:24:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2671,7 +2671,7 @@ client.on("message", message => {
         }).then(collected => {
           message.delete();
           m.delete();
-          message.member.addRole(message.guild.roles.find(c => c.name == "Activated"));
+          message.member.addRole(message.guild.roles.find(c => c.name == "Member"));
         }).catch(() => {
           m.edit(`You took to long to type the number.\nRe-type the command again if you want to verify yourself.`).then(m2 => m.delete(15000));
 });
