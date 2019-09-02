a40aeabdcab72ead2eac2410e5ce7a4588fdc967
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-09-02 17:03:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,12 +18,15 @@ const queue = new Map();
 
 const ytdl = require('ytdl-core');
 
+const fs = require('fs');
+
 const gif = require("gif-search");
 
 const Canvas = require("canvas");
 const pretty = require("pretty-ms")
 var time = require("./time.json");
 const credits = JSON.parse(fs.readFileSync('./credits.json'));
+
 
 const reportjson = JSON.parse(fs.readFileSync("./report.json", "utf8"));
 
@@ -569,7 +572,7 @@ client.on('message', message => {
               const Embed11 = new Discord.RichEmbed()
         .setColor("RANDOM")
         
-    .setDescription("Link of the server - Bot by MrBloods")
+    .setDescription("Link of the server- Bot by MrBloods")
       message.author.sendEmbed(Embed11)
     }
 });
@@ -659,7 +662,6 @@ client.on('message', message => {
     message.channel.sendEmbed(embed);
   }
 });
-
 
 client.on('message', message => {
   var prefix = "!"
@@ -2498,70 +2500,16 @@ if(e)throw e;
 }
 }); // By: FireKing , Codes
 
-client.on('message', message => {
-           if (!message.channel.guild) return;
- 
-    let room = message.content.split(" ").slice(1);
-    let findroom = message.guild.channels.find('name', `${room}`)
-    if(message.content.startsWith(prefix + "setReport")) {
-        if(!message.channel.guild) return message.reply('**This Command Only For Servers**');
-        if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('**Sorry But You Dont Have Permission** `MANAGE_GUILD`' );
-if(!room) return message.channel.send('Please Type The Channel Name')
-if(!findroom) return message.channel.send('Cant Find This Channel')
-let embed = new Discord.RichEmbed()
-.setTitle('**Done The report Code Has Been Setup**')
-.addField('Channel:', `${room}`)
-.addField('Requested By:', `${message.author}`)
-.setThumbnail(message.author.avatarURL)
-.setFooter(`${client.user.username}`)
-message.channel.sendEmbed(embed)
-reportjson[message.guild.id] = {
-channel: room,
-}
-fs.writeFile("./report.json", JSON.stringify(reportjson), (err) => {
-if (err) console.error(err)
-})
-client.on('message', message => {
- 
-    if(message.content.startsWith(`${prefix}report`)) {
-        let  user  =  message.mentions.users.first();
-      if(!message.channel.guild) return message.reply('**This Command Only For Servers**');
-    let reason = message.content.split(" ").slice(2).join(" ");
-      if(!user)  return  message.channel.send("**You didn\'t mention the user to report**")
-      if(!reason) return message.reply(`**Please provide a reason**`)
-    let findchannel = (message.guild.channels.find('name', `${reportjson[message.guild.id].channel}`))
-    if(!findchannel) return message.reply(`Error 404: The report Channel Cant Find Or Not Set To Set The report Channel Type: ${prefix}setReport`)
-    message.channel.send(`Done Thank You For Your Report Will Be Seen By The Staffs`)
-    let sugembed = new Discord.RichEmbed()
-    .setTitle('New Report !')
-    .addField('Report By:', `${message.author}`)
-    .addField('Reported User:', `${user}`)
-    .addField('Report Reason:', `${reason}`)
-    .setFooter(client.user.username)
-    findchannel.sendEmbed(sugembed)
-        .then(function (message) {
-          message.react('✅')
-          message.react('❌')
-        })
-        .catch(err => {
-            message.reply(`Error 404: The report Channel Cant Find Or Not Set To Set The report Channel Type: ${prefix}setReport`)
-            console.error(err);
-        });
-        }
-      }
-)}
-})
-
 client.on("message", message => {
-  var prefix = "!";
-  if(message.content.startsWith(prefix + "embed")) {
+  var prefix = "#";
+  if(message.content.startsWith(prefix + "msg")) {
     
 
 var color = message.content.split(" ")[1];
   var text = message.content.split(" ").slice(2);
     var tt = text.join(" ")
-  if(!color) return message.reply("Type the embed color.");
-    if(!tt) return message.reply("Type your message.");
+  if(!color) return message.reply("You need to type the color !");
+    if(!tt) return message.reply("You need to type your message !");
   let embed = new Discord.RichEmbed()
   .setColor(color)
   .setDescription(tt)
