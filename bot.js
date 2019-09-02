8199a562c1f9f5032b68339e9658108956cc6926
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-09-02 20:00:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2734,7 +2734,7 @@ client.on('message', message => {
  });
 
 client.on('message', message => {
-    if (message.content === "_createroles") {
+    if (message.content === "!cr") {
     if(!message.channel.guild) return message.channel.send('**This Command Only For Servers !**')
             if (!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(`**${message.author.username} You Dont Have** ``MANAGE_ROLES`` **Premission**`);
 
